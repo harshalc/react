d3ea7f811341bb2d75a30fe03268d9a8a6c3fb0b
harshalc89 <harshalc89@gmail.com>
2019-05-26 14:41:58 +0530

    event handling

--- day-one-app/src/LinkComponent.js
+++ day-one-app/src/LinkComponent.js
@@ -2,13 +2,44 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 class LinkComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            visited: false
+        };
+
+        this.gotClicked = this.gotClicked.bind(this);
+        this.gotPointMoved = this.gotPointMoved.bind(this);
+        this.onPointerLeave = this.onPointerLeave.bind(this);
+    }
+
+    gotClicked(event){
+        event.preventDefault();
+        console.log('I am event');
+        console.log(event);
+        console.log(event.target);
+    }
+
+    gotPointMoved(event){
+        this.setState({
+            visited: true
+          });
+    }
+
+    onPointerLeave(event){
+        this.setState({
+            visited: false
+          });
+    }
     render() {
         const linkStyle = {
-            color: this.props.color,
+            color: this.state.visited? 'blue' : this.props.color,
         }
         return (
             <li>
-            <a href={this.props.apiUrl} style={linkStyle}>{this.props.urlName}</a>
+            <a href={this.props.apiUrl} style={linkStyle} onClick={this.gotClicked} 
+            onPointerMove={this.gotPointMoved}
+            onPointerLeave={this.onPointerLeave}>{this.props.urlName}</a>
             </li>
         )
     }
